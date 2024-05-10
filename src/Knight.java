ee38366a44e58f39fa1839e841e8460c7a07d36b
Namida-wo-koeyou <142437957+Namida-wo-koeyou@users.noreply.github.com>
2024-05-10 15:01:12 -0400

    I have to select what im going to push before pressing push? impossible.

--- src/Knight.java
+++ src/Knight.java
@@ -6,6 +6,9 @@ public class Knight extends Piece {
 
     @Override
     public boolean validMove(Turn currentTurn, Board board) {
+        Piece targetPos =board.board[currentTurn.endRow][currentTurn.endCol];
+        Piece currentPos = board.board[currentTurn.startRow][currentTurn.startCol];
+        if (targetPos.color== currentPos.color){return false;}
         boolean absolutePosition = Math.abs(currentTurn.startCol - currentTurn.endCol) == 2 && Math.abs(currentTurn.startRow - currentTurn.endRow) == 1;
         boolean absolutePosition2 = Math.abs(currentTurn.startCol - currentTurn.endCol) == 1 && Math.abs(currentTurn.startRow - currentTurn.endRow) == 2;
         if (absolutePosition || absolutePosition2) {
